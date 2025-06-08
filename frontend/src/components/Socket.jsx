56a1caa6bfffc88d6d8d08c71aa2d828149a1ba3
MRROBOT4850 <113339908+MRROBOT4850@users.noreply.github.com>
2025-06-08 17:11:31 +0530

    Update Socket.jsx

--- frontend/src/components/Socket.jsx
+++ frontend/src/components/Socket.jsx
@@ -1,6 +1,6 @@
 // ek instance hai sever ke liye
 import { io } from 'socket.io-client';
-const socket = io("http://localhost:3000", {
+const socket = io("https://ghost-talk-5ofn.onrender.com/", {
   autoConnect: false, 
 });
 
